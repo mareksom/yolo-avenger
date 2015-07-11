f305477a2b00ca8611ead01262768e85cf6ab630
Marek Sommer <mareksommer7@gmail.com>
2015-07-11 20:16:35 +0200

    Dodałem zoom.

--- board/Scene.h
+++ board/Scene.h
@@ -17,8 +17,9 @@ public:
 	bool on_draw(const Cairo::RefPtr<Cairo::Context> & context) override
 	{
 		context->save();
-			context->translate(transX, transY);
-			board.draw(context, transX, transX, get_width(), get_height());
+			context->translate(-transX, -transY);
+			context->scale(zoom, zoom);
+			board.draw(context, -transX, -transX, get_width(), get_height());
 		context->restore();
 		return true;
 	}
@@ -35,7 +36,7 @@ public:
 					(double) rand() / RAND_MAX
 				);
 			last = nullptr;
-			last = &board.getAt(event->x - transX, event->y - transY);
+			last = &board.getAt((event->x + transX) / zoom, (event->y + transY) / zoom);
 			last->setColor(0, 0, 0);
 		}
 		catch(typename Board::OutOfBoundsException)
@@ -47,19 +48,38 @@ public:
 
 	virtual bool on_scroll_event(GdkEventScroll * event) override
 	{
-		const double delta = 50.0;
-		if(event->direction == GDK_SCROLL_UP)
-			transY += delta;
-		else if(event->direction == GDK_SCROLL_DOWN)
-			transY -= delta;
-		else if(event->direction == GDK_SCROLL_LEFT)
-			transX += delta;
-		else if(event->direction == GDK_SCROLL_RIGHT)
-			transX -= delta;
-		else if(event->direction == GDK_SCROLL_SMOOTH)
-			std::cout << "smooth" << std::endl;
+		if(event->state & GDK_CONTROL_MASK)
+		{
+			/* Zoom in/out */
+			if(event->direction == GDK_SCROLL_UP or event->direction == GDK_SCROLL_DOWN)
+			{
+				const double underX = (event->x + transX) / zoom;
+				const double underY = (event->y + transY) / zoom;
+				if(event->direction == GDK_SCROLL_UP)
+					zoom *= 1.2;
+				else
+					zoom /= 1.2;
+				transX = underX * zoom - event->x;
+				transY = underY * zoom - event->y;
+			}
+		}
+		else
+		{
+			/* Scroll */
+			const double delta = 50.0;
+			if(event->direction == GDK_SCROLL_UP)
+				transY -= delta;
+			else if(event->direction == GDK_SCROLL_DOWN)
+				transY += delta;
+			else if(event->direction == GDK_SCROLL_LEFT)
+				transX -= delta;
+			else if(event->direction == GDK_SCROLL_RIGHT)
+				transX += delta;
+			else if(event->direction == GDK_SCROLL_SMOOTH)
+				std::cout << "smooth" << std::endl;
+		}
 		queue_draw();
-		return Gtk::DrawingArea::on_scroll_event(event);
+		return true;
 	}
 
 private:
